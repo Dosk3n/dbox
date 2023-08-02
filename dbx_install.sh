dfc2fa2b2a004b67499ae901ad625ce4c11295e5
deantearse <dean.tearse@luxury-leisure.co.uk>
2023-08-02 10:33:10 +0100

    update

--- dbx_install.sh
+++ dbx_install.sh
@@ -18,13 +18,28 @@ fi
 
 # Install required applications
 
-sudo apt-get update
-
 # Docker
 # Remove old pkgs
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
 
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
 
 
 mkdir /mnt/local/dbox/
@@ -53,3 +68,8 @@ mkdir /mnt/local/dbox/Media/XXX/
 mkdir /mnt/local/dbox/transcodes/
 mkdir /mnt/local/dbox/transcodes/plex/
 mkdir /mnt/local/dbox/transcodes/plex/Transcode
+
+# Set ownership and permissions for the current user
+CURRENT_USER=$(logname)  # Get the username of the current user
+chown -R $CURRENT_USER:$CURRENT_USER /mnt/local  # Change ownership recursively
+chmod -R 755 /mnt/local  # Set permissions recursively
