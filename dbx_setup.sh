3e2e2c1aa9bba9b35abb783350af9d2edd762af8
deantearse <dean.tearse@luxury-leisure.co.uk>
2023-08-02 14:56:40 +0100

    changed the install process

--- dbx_setup.sh
+++ dbx_setup.sh
@@ -26,3 +26,30 @@ chmod -R 755 /srv/dbox  # Set permissions recursively
 
 # Set execution on scripts
 chmod +x /srv/dbox/git/dbx_install.sh
+
+# Install required applications
+
+# Docker
+# Remove old pkgs
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER # might need to fix this as had to run manually
+
+sudo docker network create proxy
