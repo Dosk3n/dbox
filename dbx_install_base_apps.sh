023ac691657248699ad78bd51b8f2c3b71d2696b
deantearse <dean.tearse@luxury-leisure.co.uk>
2023-08-02 12:23:05 +0100

    update

--- dbx_install_base_apps.sh
+++ dbx_install_base_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #########################################################################
-# Title:         dBox Stage 1 Install Script                            #
+# Title:         dBox Base Install Script                               #
 # Author(s):     Dosk3n                                                 #
 # URL:           https://github.com/dosk3n/dbox                         #
 # --                                                                    #
@@ -8,8 +8,7 @@
 #                   GNU General Public License v3.0                     #
 #########################################################################
 #
-# https://raw.githubusercontent.com/Dosk3n/dbox/main/dbx_setup.sh
-# curl -sL https://raw.githubusercontent.com/Dosk3n/dbox/main/dbx_setup.sh | sudo -H bash && cd /srv/dbox/git
+# Install the base apps: Plex, Sonarr, Radarr, Overseerr, NzbGet, rTorrent
 
 if [ "$EUID" -ne 0 ]; then
   echo "This script must be run with sudo or as the root user."
@@ -67,7 +66,6 @@ mkdir /mnt/local/dbox/Media/XXX/
 
 mkdir /mnt/local/dbox/transcodes/
 mkdir /mnt/local/dbox/transcodes/plex/
-mkdir /mnt/local/dbox/transcodes/plex/Transcode
 
 # Set ownership and permissions for the current user
 CURRENT_USER=$(logname)  # Get the username of the current user
